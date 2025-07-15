b58527a05d3d248d35caed95e9ffc9b25010e025
Marcus Oliveira <71191355+marcuswmc@users.noreply.github.com>
2025-07-16 00:30:35 +0100

    update: MailerSender cors

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 
 // Configurar MailerSend
